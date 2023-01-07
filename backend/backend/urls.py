6bb56163feda8bec199cc2185d06aca898ba49c3
yoc <yyyyyoc@hotmail.com>
2023-01-07 11:54:50 +0800

    Backend devs.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.views.static import serve  # media 配置
+from django.conf import settings  # media 配置
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    re_path(r'^media/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),  # media 配置
+
+    path('api/', include('api.urls')),  # `api` 的 `url`
+
 ]
