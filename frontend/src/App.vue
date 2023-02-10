e91ec44787cf19f86d93abd7c00c9f9340c7de4f
iyoc <yyyyyoc@hotmail.com>
2023-02-10 20:48:58 +0800

    font and nav fixed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,7 +16,7 @@ getApp().then(result => {
 
   <!--  导航条: 开始  -->
 <!--  <div class="backdrop-blur-md navbar text-white" style="position: fixed;">-->
-  <div class="backdrop-blur-md navbar text-white">
+  <div class="navbar text-white">
 
     <div class="navbar-start">
       <div class="dropdown">
@@ -45,7 +45,7 @@ getApp().then(result => {
       </div>
       <router-link class="w-20 content-center"
                    role="img" to="/">
-        <img :src=favicon alt="avatar" style="height: 100%">
+        <img :src=favicon alt="avatar">
       </router-link>
     </div>
 
@@ -56,7 +56,7 @@ getApp().then(result => {
           <router-link to="object-detection">
             <svg class="w-24 fill-current" viewBox="0 0 1120 534" version="1.1" xmlns="http://www.w3.org/2000/svg"
                  xml:space="preserve"
-                 style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g><g id="文字" transform="matrix(0.919561,0,0,1,-46.7656,0)"><text x="532.236px" y="435.298px" style="font-family:'SmileySans-Oblique', 'Smiley Sans';font-style:italic;font-size:450.886px;">AIIO</text></g><path
+                 style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;font-family: SmileySans"><g><g id="文字" transform="matrix(0.919561,0,0,1,-46.7656,0)"><text x="532.236px" y="435.298px" style="font-family:SmileySans;font-size:450.886px;">AIIO</text></g><path
                 id="底框"
                 d="M1093.33,50.667c0,-13.246 -11.949,-24 -26.666,-24l-1013.33,-0c-14.717,-0 -26.666,10.754 -26.666,24l-0,432c-0,13.246 11.949,24 26.666,24l1013.33,-0c14.717,-0 26.666,-10.754 26.666,-24l0,-432Zm-26.666,20.286c-0,-11.196 -10.1,-20.286 -22.541,-20.286l-621.586,-0c-12.44,-0 -22.54,9.09 -22.54,20.286l-0,391.427c-0,11.197 10.1,20.287 22.54,20.287l621.586,-0c12.441,-0 22.541,-9.09 22.541,-20.287l-0,-391.427Zm-813.334,25.158l0,-22.778c0,-11.038 -8.961,-20 -20,-20l-160,0c-11.038,0 -20,8.962 -20,20l0,253.334c0,11.038 8.962,20 20,20l29.477,-0l0,113.333c0,11.038 8.962,20 20,20l160,-0c11.039,-0 20,-8.962 20,-20l0,-70.556l50.523,0c11.039,0 20,-8.961 20,-20l0,-253.333c0,-11.038 -8.961,-20 -20,-20l-100,-0Zm-80,250.556l-43.856,-0l0,92c0,8.094 6.572,14.666 14.667,14.666l117.333,0c8.095,0 14.667,-6.572 14.667,-14.666l-0,-49.223l-82.811,0c-11.038,0 -20,-8.961 -20,-20l0,-22.777Zm80,-133.334l0,113.334c0,11.038 -8.961,20 -20,20l-33.333,-0l0,1.444c0,8.095 6.572,14.667 14.667,14.667l61.477,-0l-0,-134.778c-0,-8.095 -6.572,-14.667 -14.667,-14.667l-8.144,0Zm0,-90.555l0,63.889l29.477,-0c11.039,-0 20,8.961 20,20l0,156.111l29.19,-0c8.095,-0 14.667,-6.572 14.667,-14.667l-0,-210.667c-0,-8.095 -6.572,-14.666 -14.667,-14.666l-78.667,-0Zm-150.523,197.222l0,-113.333c0,-11.039 8.962,-20 20,-20l50.523,-0l0,-70.556c0,-11.038 8.962,-20 20,-20l33.334,-0l-0,-1.444c-0,-8.095 -6.572,-14.667 -14.667,-14.667l-117.333,-0c-8.095,-0 -14.667,6.572 -14.667,14.667l0,210.666c0,8.095 6.572,14.667 14.667,14.667l8.143,-0Zm70.523,-106.667l-29.189,0c-8.095,0 -14.667,6.572 -14.667,14.667l0,92l43.856,-0l0,-106.667Zm53.334,0l-26.667,0l0,106.667l12,-0c8.095,-0 14.667,-6.572 14.667,-14.667l-0,-92Zm-0,-90.555l-12,-0c-8.095,-0 -14.667,6.571 -14.667,14.666l0,49.223l26.667,-0l-0,-63.889Z"/></g></svg>
             <p class="text-xl text-current">Object Detection</p>
--- frontend/src/components/ObjectDetection.vue
+++ frontend/src/components/ObjectDetection.vue
@@ -6,7 +6,7 @@
         <h1>
           <svg class="w-44 fill-current mb-6" viewBox="0 0 1120 534" version="1.1" xmlns="http://www.w3.org/2000/svg"
                xml:space="preserve"
-               style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g><g id="文字" transform="matrix(0.919561,0,0,1,-46.7656,0)"><text x="532.236px" y="435.298px" style="font-family:'SmileySans-Oblique', 'Smiley Sans';font-style:italic;font-size:450.886px;">AIIO</text></g><path
+               style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;font-family: SmileySans;"><g><g id="文字" transform="matrix(0.919561,0,0,1,-46.7656,0)"><text x="532.236px" y="435.298px" style="font-family:SmileySans;font-size:450.886px;">AIIO</text></g><path
               id="底框"
               d="M1093.33,50.667c0,-13.246 -11.949,-24 -26.666,-24l-1013.33,-0c-14.717,-0 -26.666,10.754 -26.666,24l-0,432c-0,13.246 11.949,24 26.666,24l1013.33,-0c14.717,-0 26.666,-10.754 26.666,-24l0,-432Zm-26.666,20.286c-0,-11.196 -10.1,-20.286 -22.541,-20.286l-621.586,-0c-12.44,-0 -22.54,9.09 -22.54,20.286l-0,391.427c-0,11.197 10.1,20.287 22.54,20.287l621.586,-0c12.441,-0 22.541,-9.09 22.541,-20.287l-0,-391.427Zm-813.334,25.158l0,-22.778c0,-11.038 -8.961,-20 -20,-20l-160,0c-11.038,0 -20,8.962 -20,20l0,253.334c0,11.038 8.962,20 20,20l29.477,-0l0,113.333c0,11.038 8.962,20 20,20l160,-0c11.039,-0 20,-8.962 20,-20l0,-70.556l50.523,0c11.039,0 20,-8.961 20,-20l0,-253.333c0,-11.038 -8.961,-20 -20,-20l-100,-0Zm-80,250.556l-43.856,-0l0,92c0,8.094 6.572,14.666 14.667,14.666l117.333,0c8.095,0 14.667,-6.572 14.667,-14.666l-0,-49.223l-82.811,0c-11.038,0 -20,-8.961 -20,-20l0,-22.777Zm80,-133.334l0,113.334c0,11.038 -8.961,20 -20,20l-33.333,-0l0,1.444c0,8.095 6.572,14.667 14.667,14.667l61.477,-0l-0,-134.778c-0,-8.095 -6.572,-14.667 -14.667,-14.667l-8.144,0Zm0,-90.555l0,63.889l29.477,-0c11.039,-0 20,8.961 20,20l0,156.111l29.19,-0c8.095,-0 14.667,-6.572 14.667,-14.667l-0,-210.667c-0,-8.095 -6.572,-14.666 -14.667,-14.666l-78.667,-0Zm-150.523,197.222l0,-113.333c0,-11.039 8.962,-20 20,-20l50.523,-0l0,-70.556c0,-11.038 8.962,-20 20,-20l33.334,-0l-0,-1.444c-0,-8.095 -6.572,-14.667 -14.667,-14.667l-117.333,-0c-8.095,-0 -14.667,6.572 -14.667,14.667l0,210.666c0,8.095 6.572,14.667 14.667,14.667l8.143,-0Zm70.523,-106.667l-29.189,0c-8.095,0 -14.667,6.572 -14.667,14.667l0,92l43.856,-0l0,-106.667Zm53.334,0l-26.667,0l0,106.667l12,-0c8.095,-0 14.667,-6.572 14.667,-14.667l-0,-92Zm-0,-90.555l-12,-0c-8.095,-0 -14.667,6.571 -14.667,14.666l0,49.223l26.667,-0l-0,-63.889Z"/></g></svg>
         </h1>
@@ -46,6 +46,11 @@
 
 
         <!--  文件上传  -->
+        <div v-if="detStatus === 0" class="text-white mt-5">
+          <p class="text-4xl font-bold mt-10 text-secondary">Image Requirement:</p>
+          <p class="text-xl font-bold mt-3 text-warning">Format: jpg/jpeg/png & Size: smaller than (1000 * 1000)</p>
+          <p class="text-xl font-bold mt-3 mb-16 text-warning">2-virtual-core cloud-based CPU 🥲</p>
+        </div>
         <input id="imgReceive" type="file" accept=".png,.jpeg,.jpg" @change="imgUpload($event)"
                class="file-input file-input-bordered file-input-primary w-full max-w-xs"/>
         <!--  图片加载  -->
@@ -91,9 +96,8 @@
               <path class="opacity-75" fill="currentColor"
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
-            Detecting, please wait...
-            <p>It should be done in 10s.</p>
-            <p>2-virtual-core cloud-based CPU 🥲</p>
+            Detecting, please wait... It should be done in 10s.
+
           </div>
           <div v-if="detStatus === 2" class="text-lg font-bold mt-3 text-error">
             Detected, {{ time_cost }}s spent.
@@ -101,12 +105,12 @@
         </div>
 
         <!--  CANVAS  -->
-        <canvas v-if="doneLoad !== 0" id="canvas"
+        <canvas v-if="doneLoad !== 0 && sizeCheck" id="canvas"
                 class="bg-neutral rounded-lg fill-current mt-8 max-w-screen-2xl max-h-screen" :width=width
                 :height=height>
         </canvas>
 
-        <button v-if="doneLoad !== 0" class="btn btn-primary text-3xl font-bold  mt-10" @click="postImage">DETECT!
+        <button v-if="doneLoad !== 0 && sizeCheck" class="btn btn-primary text-3xl font-bold  mt-10" @click="postImage">DETECT!
         </button>
 
 
@@ -173,6 +177,8 @@ function imgUpload(event) {
 
 }
 
+var sizeCheck = ref(1);
+
 function getImgDisplay() {
 
   let img = new Image();
@@ -180,6 +186,10 @@ function getImgDisplay() {
   height.value = img.height;
   width.value = img.width;
   doneLoad.value += 1;
+
+  if (height.value > 1000 || width.value > 1000) {
+    sizeCheck.value = 0;
+  }
 
   // console.log(height.value, width.value);
 
